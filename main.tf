9036cda4ba604a216f6ea7229c5818ae6eac3dba
root <root@ip-172-31-45-87.ap-south-1.compute.internal>
2023-02-17 13:39:07 +0000

    my terraform file

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region      = "ap-south-1"
+  access_key  = "AKIAWO5LHJ53LHS74MM6"
+  secret_key  = "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"
+}
+
+resource "aws_s3_bucket" "exmaple1" {
+   bucket     = "krishna905"
+}
+
